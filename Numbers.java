2334a29e2c12dd6b2eea4d4b07cad4ba9eb22781
fathmi sathar <96736316+fathmi-sathar@users.noreply.github.com>
2024-06-17 20:14:09 +0530

    sum of 2 no.s

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Numbers
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the first number");
+    int n1=sc.nextInt();
+    System.out.println("Enter the second number");
+    int n2=sc.nextInt();
+    System.out.println("THe sum of numbers is"+"\t"+(n1+n2));
+    }
+    }
